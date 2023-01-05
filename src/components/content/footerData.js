25e40f4d5d4dce9d0cecea08fbdf3bedced00dd3
DavOlufuwa <davolufuwa@gmail.com>
2023-01-05 06:33:14 +0100

    added content files

--- src/components/content/footerData.js
+++ src/components/content/footerData.js
@@ -1,39 +1,25 @@
 
-export const footerData = [
+export const testimonials = [
     {
         id:"00",
-        topic : [
-            'The player',
-            'Overview',
-            'Community',
-            'Download',
-            'Fanzone'
-        ]
+        testimony: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam interdum nisl et nunc facilisis, a commodo eros mollis. Nunc vel pellentesque est. Curabitur at odio sit amet libero vulputate efficitur ac nec justo. Nulla vitae mauris quam. Nulla quam massa, faucibus id pretium ac, mattis eu velit. Donec sed risus a lacus fringilla finibus.',
+        image : require('../../assets/images/harry-wilson.svg').default,
+        title : 'Harry Wilson',
+        description : 'Property Owner'
     },
     {
         id:"01",
-        topic:[
-            'Creators',
-            'Overview',
-            'Services',
-            'Pricing',
-            'Fanzone'
-        ]
-         
+        testimony: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Nihil in perspiciatis perferendis autem libero vero quae sint voluptates tenetur repudiandae provident amet architecto itaque, facere rem voluptatibus quaerat sed alias.',
+        image : require('../../assets/images/maria-knox.svg').default,
+        title : 'Maria Knox',
+        description : 'Property Owner'
     },
     {
-        id:"02 ",
-        topic:[
-            'Company',
-            'About us',
-            'Careers',
-            'Foundation',
-            'Help Initiative',
-            'For Partners',
-            'Press Kit',
-            'Tribune'
-        ]
+        id:"02",
+        testimony: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Provident voluptates maxime est libero, at, molestias aspernatur odio fugit nulla magnam quos maiores distinctio, doloribus ipsa dolores enim!',
+        image : require('../../assets/images/bob-bellerin.svg').default,
+        title : 'Bob Bellerin',
+        description : 'Apartment Owner'
     }
-
-
 ]
+
---
+++ src/components/content/propertyListData.js
@@ -0,0 +1,57 @@
+
+export const propertyListData = [
+    {
+        image: require('../../assets/images/property-one.png').default,
+        address : '2578 Folsom street, san francisco, CA, 94110',
+        roomType : 'Private Room',
+        rent : '$1200/month',
+        bed: '4',
+        shower : '2',
+        size : '2'       
+    },
+    {
+        image: require('../../assets/images/property-two.png').default,
+        address : '2578 Folsom street, san francisco, CA, 94110',
+        roomType : 'Private Room',
+        rent : '$1200/month',
+        bed: '4',
+        shower : '2',
+        size : '2'       
+    },
+    {
+        image: require('../../assets/images/property-three.png').default,
+        address : '2578 Folsom street, san francisco, CA, 94110',
+        roomType : 'Private Room',
+        rent : '$1200/month',
+        bed: '4',
+        shower : '2',
+        size : '2'       
+    },
+    {
+        image: require('../../assets/images/property-four.png.png').default,
+        address : '2578 Folsom street, san francisco, CA, 94110',
+        roomType : 'Private Room',
+        rent : '$1200/month',
+        bed: '4',
+        shower : '2',
+        size : '2'       
+    },
+    {
+        image: require('../../assets/images/property-five.png').default,
+        address : '2578 Folsom street, san francisco, CA, 94110',
+        roomType : 'Private Room',
+        rent : '$1200/month',
+        bed: '4',
+        shower : '2',
+        size : '2'       
+    },
+    {
+        image: require('../../assets/images/property-six.png').default,
+        address : '2578 Folsom street, san francisco, CA, 94110',
+        roomType : 'Private Room',
+        rent : '$1200/month',
+        bed: '4',
+        shower : '2',
+        size : '2'       
+    },
+]
