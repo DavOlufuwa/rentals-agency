d487ead99c75c41598fce4d5916f9d1242c0a7b1
DavOlufuwa <davolufuwa@gmail.com>
2023-01-07 18:18:05 +0100

    started on the forms

--- src/components/Propertyform/Propertyform.js
+++ src/components/Propertyform/Propertyform.js
@@ -2,17 +2,30 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import React from 'react'
 import './Propertyform.scss'
 import { faStarOfLife } from '@fortawesome/free-solid-svg-icons'
+import { useDropzone } from 'react-dropzone'
 
 const Propertyform = () => {
+
+    // Image upload code
+    const { getRootProps, getInputProps } = useDropzone({
+        accept: 'image/*',
+        onDrop: acceptedFiles => {
+          return
+        },
+      });
+    // Image upload code
+
+
   return (
     <div className="form-bg">
         <div className="title">
-
+            Your property with us and be Confident that Your Room will be Filled Out!
         </div>
         <div className="form">
-            <div className="title">
+            <div className="form-title">
+                Add A New Property
             </div>
-            <form action="">
+            <form action="" className='property-form'>
             <div className="form-control">
                 <label htmlFor="name">Name
                     <FontAwesomeIcon
@@ -55,7 +68,7 @@ const Propertyform = () => {
                     />
                 </label>
                 <select name="city" id="">
-                    <option value="" disabled>Select City</option>
+                    <option value="">Select City</option>
                     <option value="Abuja">Abuja</option>
                     <option value="Dubai">Dubai</option>
                 </select>
@@ -68,7 +81,7 @@ const Propertyform = () => {
                     />
                 </label>
                 <select name="state" id="">
-                    <option value="" disabled>Select State</option>
+                    <option value="">Select State</option>
                     <option value="Abuja">Abuja</option>
                     <option value="Dubai">Dubai</option>
                 </select>
@@ -81,7 +94,7 @@ const Propertyform = () => {
                     />
                 </label>
                 <select name="state" id="">
-                    <option value="" disabled>Select Room Type</option>
+                    <option value="">Select Room Type</option>
                     <option value="Abuja">Self contained</option>
                     <option value="Dubai">Room and Parlour</option>
                 </select>
@@ -94,21 +107,49 @@ const Propertyform = () => {
                     />
                 </label>
                 <input type="text" 
-                    placeholder='enter name'
+                    placeholder='enter price'
                 />
             </div>
             <div className="form-control">
-                <label htmlFor="name">Name
+                <label htmlFor="state">Room Type
                     <FontAwesomeIcon
                         icon={faStarOfLife}
                         className="required"
                     />
                 </label>
-                <input type="text" 
-                    placeholder='enter name'
-                />
+                <select name="state" id="">
+                    <option value="">Select Room Type</option>
+                    <option value="Abuja">Self contained</option>
+                    <option value="Dubai">Room and Parlour</option>
+                </select>
+            </div>
+            <div className="form-control">
+                <label htmlFor="description">Description
+                    <FontAwesomeIcon
+                        icon={faStarOfLife}
+                        className="required"
+                    />
+                </label>
+                <textarea name="description" placeholder='Enter Description'>
+                    
+                </textarea>
+            </div>
+            <div className="form-control-upload">
+                <label htmlFor="upload">
+                    upload files
+                </label>
+                <div {...getRootProps()}>
+                    <input {...getInputProps()} />
+                    <p>Drag 'n' drop some files here, or <span>Browse</span> </p>
+                    <p>Supported JPG, JPEG, PNG</p>
+                </div>
             </div>
             </form>
+            <div className="btn">
+                <button>
+                    Add New Property
+                </button>
+            </div>
         </div>
     </div>
   )
